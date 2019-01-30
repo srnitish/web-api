3c9b15a5839cb0776fe07bac55f7bb36c5e37fc8
srnitish <sr.nitish06@gmail.com>
2019-01-30 16:12:18 +0530

    changes

---
+++ script.js
@@ -0,0 +1,56 @@
+// Javascript code
+function doFirst(){
+	var button = document.getElementById("button");
+	button.addEventListener("click", saveData, false);
+	display();
+}
+
+function saveData(){
+
+	var one = document.getElementById("one").value;
+	var two = document.getElementById("two").value;
+	sessionStorage.setItem(one,two);
+	alert("Record Added!!")
+	display();
+	document.getElementById("one").value = "";
+	document.getElementById("two").value = "";	
+	}
+
+
+function deleteData(){
+	if(sessionStorage.key("key") === document.getElementById("one").value) {
+		alert("Match found, record deleting!!");
+		sessionStorage.removeItem(one);
+		display();
+	}
+	for(var x=0; x<sessionStorage.length; x++){
+		var a = sessionStorage.key(x);
+		var b = sessionStorage.getItem(a);	
+		if(sessionStorage.key(x) != document.getElementById("one").value){
+			alert("Match not found, try to delete matched record!!");
+		}
+	}
+	// event.preventdefault();
+	var one = document.getElementById("one").value;
+	var two = document.getElementById("two").value;
+	sessionStorage.removeItem(one,two);
+	rightbox.innerHTML = "";
+	display();
+	document.getElementById("one").value = "";
+	document.getElementById("two").value = "";
+}
+
+function display(){
+	var rightbox = document.getElementById("rightbox");
+	rightbox.innerHTML = "";
+	for(var x=0; x<sessionStorage.length; x++){
+		var a = sessionStorage.key(x);
+		var b = sessionStorage.getItem(a);
+		rightbox.innerHTML += "Name: " + a + " | Value is: " + b + "<br/>";
+	}
+	// var two = sessionStorage.getItem(one);
+	// rightbox.innerHTML = "Name of variable: " +one + "<br > Value is: " +two;
+}
+
+window.addEventListener("load", doFirst, false);
+window.addEventListener("btnClick", deleteData, false);
