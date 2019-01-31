69008777d0f569f9c169fd2e4eb7c17cec1a6800
srnitish <sr.nitish06@gmail.com>
2019-01-31 14:22:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function doFirst(){
 	display();
 }
 
+// peice of code to search data availability
 function checkData(){
 	var one = document.getElementById("one").value; 
 	for(var x = 0; x<sessionStorage.length; x++){
@@ -22,18 +23,20 @@ function checkData(){
 
 	if(one != a && b){
 		alert("Record not found!!");
+		document.getElementById("one").value = "";
+		document.getElementById("two").value = "";
 		return one;
 	}
 
-	
-	
-	// if(document.getElementById("one").value == ""){
-	// 	alert("Enter something to search record!!");
-	// 	sessionStorage.removeItem(one);
-	// }
+	if(document.getElementById("one").value == ""){
+		alert("Enter something to search in the storage!!");
+	}
 }
+// peice of code to search data availability
 
 
+
+// peice of code to save data
 function saveData(){
 	var one = document.getElementById("one").value; 
 	for(var x = 0; x<sessionStorage.length; x++){
@@ -43,6 +46,7 @@ function saveData(){
 			alert("Record already exists, Please save another record!!");
 			document.getElementById("one").value = "";
 			document.getElementById("two").value = "";
+			return one;
 		}
 	}
 
@@ -60,19 +64,55 @@ function saveData(){
 	document.getElementById("two").value = "";
 	}
 }
+// peice of code to save data
 
+
+// peice of code to delete data
 function deleteData(){
 	if(document.getElementById("one").value == ""){
 		alert("Enter something to delete record!!");
 		sessionStorage.removeItem(one);
-	}
-		
-	if(sessionStorage.key("key") === document.getElementById("one").value) {
-		alert("Match found, record deleting!!");
-		sessionStorage.removeItem(one);
-		display();
+		return one;
 	}
 
+	var one = document.getElementById("one").value; 
+	for(var x = 0; x<sessionStorage.length; x++){
+		var a = sessionStorage.key(x);
+		var b = sessionStorage.getItem(a);
+		if(one == a && b){
+			alert("Match found, record deleting!!");
+			sessionStorage.removeItem(one);
+			document.getElementById("one").value = "";
+			document.getElementById("two").value = "";
+			display();
+			return one;
+		}
+	}
+
+	if(document.getElementById("one").value != sessionStorage.key("key")) {
+		alert("Match not found, to delete anything!!");
+		sessionStorage.removeItem(one);
+		document.getElementById("one").value = "";
+		document.getElementById("two").value = "";
+	}
+
+	
+
+	// if(document.getElementById("one").value == sessionStorage.key("key")) {
+	// 	alert("Match found, record deleting!!");
+	// 	sessionStorage.removeItem("key");
+	// 	display();
+	// }
+
+	
+
+	// if(document.getElementById("one").value != sessionStorage.key("key")){
+	// 	alert("Match does not exist in the session storage!!");
+	// 	document.getElementById("one").value = "";
+	// 	document.getElementById("two").value = "";
+	// 	return one;
+	// }
+		
 	var one = document.getElementById("one").value;
 	var two = document.getElementById("two").value;
 	sessionStorage.removeItem(one,two);
@@ -81,7 +121,10 @@ function deleteData(){
 	document.getElementById("one").value = "";
 	document.getElementById("two").value = "";
 }
+// peice of code to delete data
 
+
+// peice of code to display data
 function display(){
 	var rightbox = document.getElementById("rightbox");
 	rightbox.innerHTML = "";
@@ -90,9 +133,9 @@ function display(){
 		var b = sessionStorage.getItem(a);
 		rightbox.innerHTML += "key: " + a + " | value is: " + b + "<br/>" + "<hr/>";
 	}
-	// var two = sessionStorage.getItem(one);
-	// rightbox.innerHTML = "Name of variable: " +one + "<br > Value is: " +two;
 }
+// peice of code to display data
+
 
 window.addEventListener("load", doFirst, false);
 window.addEventListener("btnClick", deleteData, false);
